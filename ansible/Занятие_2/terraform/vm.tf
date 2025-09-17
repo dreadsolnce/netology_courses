030b82e16e6c91827d078061ef369f6cb835e76b
Владимир <you@example.com>
2025-09-17 18:52:37 +0300

    Фтышиду Pflfybt 2

---
+++ ansible/Занятие_2/terraform/vm.tf
@@ -0,0 +1,44 @@
+data "yandex_compute_image" "ubuntu" {
+  family = "ubuntu-2004-lts"
+}
+
+# Создание первых двух виртуальных машин
+resource "yandex_compute_instance" "web" {
+  count       = 1
+  # name        = "${var.nameVMWeb}-${count.index+1}"
+  name        = join("", [var.nameVMWeb, "-${count.index+1}"])
+  zone        = var.default_zone
+  platform_id = "standard-v3"
+  hostname    = join("", [var.nameVMWeb, "-${count.index+1}"])
+
+  resources {
+    cores = var.resourcesVMWeb.cores
+    memory = var.resourcesVMWeb.memory
+    core_fraction = var.resourcesVMWeb.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu.image_id
+    }
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.develop.id
+    security_group_ids = [yandex_vpc_security_group.example.id]
+    nat       = true
+  }
+
+  metadata = {
+    serial-port-enable = 1
+    # Через файл открытого ключа pub на локальной машине
+    ssh-keys = "ubuntu:${local.ssh_pub_key}"
+    # Через локальную переменную с текстом открытого ключа pub
+    # ssh-keys           = local.ssh_key
+  }
+}
+
