ed23e24f7f4f8d72dfa6dfcb7d788caf43c7dd29
Владимир <you@example.com>
2025-10-15 18:01:03 +0300

    teamcity

---
+++ teamcity/terraform/providers.tf
@@ -0,0 +1,45 @@
+terraform {
+
+  required_version = ">=1.9.8"
+
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.157.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = file(var.service_account_key_file)
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone #(Optional)
+}
+  # backend "s3" {
+  #   endpoints = {
+  #     # База данных для хранения блокировок (делается отдельно)
+  #     dynamodb = "https://docapi.serverless.yandexcloud.net/ru-central1/b1gr160bk1vuruuer3om/etn5fc5eukelv6gk3uul"
+  #     # Хранение файла terraform.tfstate
+  #     s3 = "https://storage.yandexcloud.net"
+  #   }
+  #
+  #   dynamodb_table = "table_for_save_lockfile_tfstate"
+  #
+  #   shared_credentials_files = ["~/.aws/credentials"]
+  #   profile = "default"
+  #   region  = "ru-central1"
+  #
+  #   bucket = "tfstate-kvl"
+  #   key    = "catalog/terraform.tfstate"
+  #
+  #   skip_region_validation      = true
+  #   skip_credentials_validation = true
+  #   skip_requesting_account_id  = true
+  #   skip_s3_checksum            = true
+  # }
---
+++ teamcity/terraform/variables.tf
@@ -0,0 +1,80 @@
+###cloud vars
+variable "cloud_id" {
+  type        = string
+  default     = "b1gr160bk1vuruuer3om"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1gh1tm69ej6s5k9qube"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "service_account_key_file" {
+  type        = string
+  default     = "~/keys/authorized_key_docker_swarm.json"
+  description = "Путь к ключу для подключения к сервисному акаунту"
+}
+
+variable "ssh_pub_key" {
+  default  = "~/.ssh/id_rsa.pub"
+}
+######################## переменные сети ##############################################
+variable "zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "Зона доступности"
+}
+
+variable "cidr_net" {
+  type        = string
+  default     = "10.0.0.0/24"
+  description = "Диапазон ip адресов внутренней сети"
+}
+
+variable "vpc_name" {
+   type        = string
+   default     = "vpc_teamcity"
+   description = "Название сети"
+}
+
+variable "vpc_subnet_name" {
+  type        = string
+  default     = "subnet-teamcity"
+  description = "Название подсети"
+}
+
+variable "imageID" {
+  type        = string
+  default     = "fd8d464f5srt40f0mftt"
+  description = "id образа виртуальной машины"
+}
+
+variable "instance_name" {
+  type        = list(string)
+  default     = ["teamcity-server", "teamcity-agent", "nexus"]
+  description = "Имена создаваемых машин"
+}
+
+variable "type-vm" {
+  type            = string
+  default         = "standard-v3"
+  description     = "Тип создаваемой виртуальной машины"
+}
+
+variable "resourcesVM" {
+  type = object(
+    {
+      cores = number
+      memory: number
+      core_fraction: number
+    }
+  )
+  default = {
+    cores         = 2
+    memory        = 4
+    core_fraction = 100
+  }
+  description = "Ресурсы для создания первых двух машин"
+}
