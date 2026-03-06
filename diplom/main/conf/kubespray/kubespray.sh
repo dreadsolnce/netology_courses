19124870f0eadb397f6bee2f6831ab34ee60fb31
Владимир <you@example.com>
2026-03-06 18:37:25 +0300

    еще правочка

--- diplom/main/conf/kubespray/kubespray.sh
+++ diplom/main/conf/kubespray/kubespray.sh
@@ -9,12 +9,12 @@ cd /tmp
 echo "Скачиваем и устанавливаем pyenv"
 curl https://pyenv.run | /bin/bash
 
-#echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-#echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-#echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+#export PYENV_ROOT="$HOME/.pyenv"
+#export PATH="$PYENV_ROOT/bin:$PATH"
+#eval "$(pyenv init -)"
 
 #source ~/.bashrc
 
