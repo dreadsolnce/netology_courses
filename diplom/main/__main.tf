41269ed4f4ac7d1b87d76cd85a88d1f011fa75f8
Владимир <you@example.com>
2026-03-13 14:07:55 +0300

    sensitivy

--- diplom/main/__main.tf
+++ diplom/main/__main.tf
@@ -119,9 +119,9 @@ data "template_file" "cloudinit-bastion" {
     file_ssh_public_key       = filebase64(var.ssh_public_key)
     file_terraformrc          = filebase64("${abspath(path.module)}/conf/terraform/.terraformrc")
     file_connect              = filebase64("${abspath(path.module)}/conf/gitlab/connect.sh")
-    file_privkey_cloud              = sensitive(fileexists(var.file_privkey) ? filebase64(var.file_privkey) : "")
-    file_fullchain_cloud            = sensitive(fileexists(var.file_fullchain) ? filebase64(var.file_fullchain) : "")
-    file_chain_cloud                = sensitive(fileexists(var.file_chain) ? filebase64(var.file_chain) : "")
+    file_privkey_cloud              = sensitive(try(filebase64(var.file_privkey), ""))
+    file_fullchain_cloud            = sensitive(try(filebase64(var.file_fullchain), ""))
+    file_chain_cloud                = sensitive(try(filebase64(var.file_chain), ""))
   }
 }
 
